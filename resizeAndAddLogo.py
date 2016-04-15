565ef22716bd5adb2211e82aacf546183ef79c1e
Brian Torres <btorres33@mail.valenciacollege.edu>
2016-04-15 10:49:25 -0500

    2nd attempted
    figured it out

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -8,25 +8,26 @@ logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
 
 # Loop over all files in the working directory.
+os.makedirs('withLogo', exist_ok=True)
 for filename in os.listdir('.'):
     if not (filename.endswith('.png') or filename.endswith('.jgp')) \
        or filename == LOGO_FILENAME:
-        continue # skip non-image files and the logo file itself
+        continue    # skip non-image files and the logo file itself
 
-    im = Image.open(open(filename)
+    im = Image.open(filename)
+    width, height = im.size
 # Check if image needs to be resized.
 
-if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-    # Calculate the new width and height to resize to.
-    if width > height:
-        height = int((SQUARE_FIT_SIZE / width) * height)
-        width = SQUARE_FIT_SIZE
-    else:
-        width = int((SQUARE_FIT_SIZE / height) * width)
-        height = SQUARE_FIT_SIZE
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
     # Resize the image.
     print(' Resizing %s...' % (filename))
-        im = im.resize((width, height))
+    im = im.resize((width, height))
     # Add the logo
     print('Adding logo to %s...' % (filename))
     im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
